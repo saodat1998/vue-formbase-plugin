6c8f7bdbce3f1a90e3572ae24bcca40955357367
saodat <saodat.9808@gmail.com>
2020-12-11 22:04:20 +0500

    experimenting

--- src/components/Form/DefaultForm.vue
+++ src/components/Form/DefaultForm.vue
@@ -1,11 +1,6 @@
 <template>
-  <v-card
-    color="success"
-    icon="mdi-account"
-    :title="`${isUpdate ? titleUpdate : titleCreate} `"
-    class="px-5 py-3 mb-10"
-    style="margin-top: 30px;"
-  >
+  <v-card color="success" class="px-5 py-3 mb-10" style="margin-top: 30px;">
+    <h6 :v-html="`${isUpdate ? titleUpdate : titleCreate} `" />
     <form-base
       v-model="formValue"
       :schema="schema"
@@ -17,81 +12,82 @@
   </v-card>
 </template>
 <script>
-  import FormBase from './FormBase'
+import FormBase from "./FormBase";
 
-  export default {
-    name: 'DefaultForm',
-    components: {
-      FormBase,
+export default {
+  name: "DefaultForm",
+  components: {
+    FormBase,
+  },
+  props: {
+    baseUrl: {
+      type: String,
+      default: "",
     },
-    props: {
-      baseUrl: {
-        type: String,
-        default: '',
-      },
-      titleCreate: {
-        type: String,
-        default: '',
-      },
-      titleUpdate: {
-        type: String,
-        default: '',
-      },
-      nextRouteName: {
-        type: String,
-        default: '',
-      },
+    titleCreate: {
+      type: String,
+      default: "",
     },
-    data: () => ({
-      formValue: null,
-      schema: [],
-    }),
-    computed: {
-      isUpdate () {
-        return !!this.$route.params.id
-      },
-      method () {
-        return this.isUpdate ? 'put' : 'post'
-      },
-      id () {
-        return this.$route.params.id
-      },
-      formUrl () {
-        return this.isUpdate ? `${this.baseUrl}/${this.id}/edit` : `${this.baseUrl}/create`
-      },
-      endPointUrl () {
-        return this.isUpdate ? `${this.baseUrl}/${this.id}` : this.baseUrl
-      },
+    titleUpdate: {
+      type: String,
+      default: "",
     },
-    created () {
-      this.fetchForm()
+    nextRouteName: {
+      type: String,
+      default: "",
     },
-    methods: {
-      actionMethod (funcName, item) {
-        this[funcName](item)
-      },
-      fetchForm () {
-        this.axios.get(this.formUrl)
-          .then(({ data }) => {
-            this.schema = data.form
-          })
-      },
-      createOrUpdate ({ resolve }) {
-        this.axios[this.method](this.endPointUrl, this.formValue)
-          .then(({ data }) => {
-            this.$store.commit('successMessage', data.message)
+  },
+  data: () => ({
+    formValue: null,
+    schema: [],
+  }),
+  computed: {
+    isUpdate() {
+      return !!this.$route.params.id;
+    },
+    method() {
+      return this.isUpdate ? "put" : "post";
+    },
+    id() {
+      return this.$route.params.id;
+    },
+    formUrl() {
+      return this.isUpdate
+        ? `${this.baseUrl}/${this.id}/edit`
+        : `${this.baseUrl}/create`;
+    },
+    endPointUrl() {
+      return this.isUpdate ? `${this.baseUrl}/${this.id}` : this.baseUrl;
+    },
+  },
+  created() {
+    this.fetchForm();
+  },
+  methods: {
+    actionMethod(funcName, item) {
+      this[funcName](item);
+    },
+    fetchForm() {
+      this.axios.get(this.formUrl).then(({ data }) => {
+        this.schema = data.form;
+      });
+    },
+    createOrUpdate({ resolve }) {
+      this.axios[this.method](this.endPointUrl, this.formValue)
+        .then(({ data }) => {
+          this.$store.commit("successMessage", data.message);
 
-            if (this.nextRouteName) {
-              this.$router.push({ name: this.nextRouteName })
-            }
-          })
-          .catch((error) => {
-            const { response } = error
-            this.$store.commit('errorMessage', response.data.message)
-            console.error(error)
-          })
-        resolve()
-      },
+          if (this.nextRouteName) {
+            this.$router.push({ name: this.nextRouteName });
+          }
+        })
+        .catch((error) => {
+          const { response } = error;
+          this.$store.commit("errorMessage", response.data.message);
+          console.error(error);
+        });
+      resolve();
     },
-  }
+  },
+};
 </script>
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,13 +2,16 @@ import Vue from "vue";
 import FormBase from "./FormBase.vue";
 import DefaultForm from "./DefaultForm.vue";
 
+// export FormBase from "./FormBase.vue";
+// export DefaultForm from "./DefaultForm.vue";
+
 const Components = {
-    FormBase,
-    DefaultForm
+  FormBase,
+  DefaultForm,
 };
 
-Object.keys(Components).forEach(name => {
-    Vue.component(name, Components[name]);
+Object.keys(Components).forEach((name) => {
+  Vue.component(name, Components[name]);
 });
 
 export default Components;
